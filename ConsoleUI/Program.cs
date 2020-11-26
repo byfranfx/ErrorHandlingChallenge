84d36ced911f52170c8081063cdd66b299e53aba
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-11-26 19:39:09 +0100

    ErrorHandlingChallenge, done!
    Task completed succesfully!

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,9 +14,32 @@ namespace ConsoleUI
             PaymentProcessor paymentProcessor = new PaymentProcessor();
             for (int i = 0; i <= 10; i++)
             {
-                var result = paymentProcessor.MakePayment($"Demo{ i }", i);
-
-                Console.WriteLine(result.TransactionAmount);
+                TransactionModel result = null;
+                try
+                {
+                    result = paymentProcessor.MakePayment($"Demo{ i }", i);
+                    if (result != null)
+                    {
+                        Console.WriteLine(result.TransactionAmount);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Null value for item");
+                    }
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Skipped invalid record { ex.InnerException?.Message }");
+                }
+                catch (FormatException ex) when (i != 5)
+                {
+                    Console.WriteLine($"Formatting Issue { ex.InnerException?.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Payment skipped for payment with { i } items { ex.InnerException.Message }");
+                }
+                
             }
             Console.ReadLine();
         }
